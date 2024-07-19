36b382ad3947c5e4577c0276a1b0fd6a13eb65ea
Jack <5182053+phyzical@users.noreply.github.com>
2024-07-19 22:26:26 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,11 +1,24 @@
 <?php
-$app->get('/plugins/autoOrganizr/settings', function ($request, $response, $args) {
+$app->get('/plugins/autoorganizr/settings', function ($request, $response, $args) {
 	$autoOrganizrPlugin = new autoOrganizrPlugin();
-	if ($autoOrganizrPlugin->checkRoute($request)) {
-		if ($autoOrganizrPlugin->qualifyRequest(1, true)) {
-			$GLOBALS['api']['response']['data'] = autoOrganizrPlugin->_autoOrganizrPluginGetSettings();
-		}
+	$response->getBody()->write(jsonE($GLOBALS['api']));
+	return $response
+		->withHeader('Content-Type', 'application/json;charset=UTF-8')
+		->withStatus($GLOBALS['responseCode']);
+});
+$app->get('/plugins/autoorganizr/launch', function ($request, $response, $args) {
+	$sonarrThrottlingPlugin = new autoOrganizrPlugin();
+	if ($sonarrThrottlingPlugin->checkRoute($request)) {
+		$sonarrThrottlingPlugin->_autoOrganizrPluginLaunch();
 	}
+	$response->getBody()->write(jsonE($GLOBALS['api']));
+	return $response
+		->withHeader('Content-Type', 'application/json;charset=UTF-8')
+		->withStatus($GLOBALS['responseCode']);
+});
+$app->get('/plugins/autoorganizr/synctabs', function ($request, $response, $args) {
+	$autoOrganizrPlugin = new autoOrganizrPlugin();
+	$autoOrganizrPlugin->_autoOrganizrPluginSyncTabs();
 	$response->getBody()->write(jsonE($GLOBALS['api']));
 	return $response
 		->withHeader('Content-Type', 'application/json;charset=UTF-8')
