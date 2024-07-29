0a4ffca39a759629df10d9e31fbabc0876760a44
phyzical <5182053+phyzical@users.noreply.github.com>
2024-07-29 21:27:46 +0800

    log

--- main.js
+++ main.js
@@ -54,7 +54,8 @@ function AutoOrganizrPluginSyncTabs() {
 		const items = data.map(update => `<tr><th>${update.name}</th><th>${update.type}</th></tr>`).join("");
 		$('#autoOrganizrTableList table').append(items);
 		$('#autoOrganizrTableList').removeClass("hidden")
-	}).fail(function ({ response: { data, message } }) {
+	}).fail(function (xhr) {
+		console.dir(xhr)
 		$('.loadingautoOrganizr').remove();
 		$('#autoOrganizrTableList table').append(`<h3>Error: ${message}</h3>`);
 		const items = data.map(update => `<tr><th>${update.name}</th><th>${update.type}</th></tr>`).join("");
